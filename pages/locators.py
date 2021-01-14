38e5e2c26666c6d9635491484eb40f093a1bd2bf
VetalR <spector30000@gmail.com>
2021-01-14 20:43:04 +0300

    I am do it

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.ID, "login_link")
+
+class LoginPageLocators():
+    REGISTER_LINK = (By.ID, "login_link")
+    LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form.well")
+    REGISTER_FORM = (By.ID, "register_form")
--- pages/main_page.py
+++ pages/main_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 
 
+
 class MainPage(BasePage): 
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.ID, "login_link")
