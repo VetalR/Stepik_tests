dd7314406261300f50036f1f4931c1f61c9319aa
VetalR <spector30000@gmail.com>
2021-01-24 16:20:11 +0300

    add pytest.ini

--- test_product_page.py
+++ test_product_page.py
@@ -4,80 +4,84 @@ from .pages.login_page import LoginPage
 import pytest
 
 
-# def test_guest_cant_see_success_message(self, browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#     page.open()                      # открываем страницу
-#     page.should_not_be_disappeared_success_message()    # будет ждать до тех пор, пока элемент не исчезнет
 
-# def test_guest_can_add_product_to_basket(self, browser):
-#     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#     page.open()                      # открываем страницу
-#     page.add_to_basket()          # добавление книги
-#     page.solve_quiz_and_get_code()      # Работа с assert
-#     page.should_be_name_equal()     #сравнение наименований
-#     page.should_be_price_equal()        #сравнение цен
+def test_guest_cant_see_success_message(self, browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.should_not_be_disappeared_success_message()    # будет ждать до тех пор, пока элемент не исчезнет
 
-
-# def test_add_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#     page.open()                      # открываем страницу
-#     page.add_to_basket()          # добавление книги
-#     page.solve_quiz_and_get_code()      # Работа с assert
-#     page.should_be_name_equal()     #сравнение наименований
-#     page.should_be_price_equal()        #сравнение цен
-
-# @pytest.mark.parametrize('linked', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4","?promo=offer5",  
-#                                     "?promo=offer6",pytest.param("?promo=offer7", marks=pytest.mark.xfail), "?promo=offer8", "?promo=offer9"])
-# def test_add_special(browser, linked):
-#    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{linked}'
-#    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#    page.open()                      # открываем страницу
-#    page.add_to_basket()          # добавление книги
-#    page.solve_quiz_and_get_code()      # Работа с assert
-#    page.should_be_name_equal()     #сравнение наименований
-#    page.should_be_price_equal()        #сравнение цен
-
-# @pytest.mark.xfail
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#     page.open()                      # открываем страницу
-#     page.add_to_basket()          # добавление книги
-#     page.should_not_be_success_message()    # упадет, как только увидит искомый элемент
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.add_to_basket()          # добавление книги
+    page.solve_quiz_and_get_code()      # Работа с assert
+    page.should_be_name_equal()     #сравнение наименований
+    page.should_be_price_equal()        #сравнение цен
 
 
+def test_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.add_to_basket()          # добавление книги
+    page.solve_quiz_and_get_code()      # Работа с assert
+    page.should_be_name_equal()     #сравнение наименований
+    page.should_be_price_equal()        #сравнение цен
 
-# @pytest.mark.xfail
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#     page.open()                      # открываем страницу
-#     page.add_to_basket()          # добавление книги
-#     page.should_not_be_disappeared_success_message()    # будет ждать до тех пор, пока элемент не исчезнет
+@pytest.mark.parametrize('linked', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4","?promo=offer5",  
+                                    "?promo=offer6",pytest.param("?promo=offer7", marks=pytest.mark.xfail), "?promo=offer8", "?promo=offer9"])
+def test_add_special(browser, linked):
+   link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{linked}'
+   page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+   page.open()                      # открываем страницу
+   page.add_to_basket()          # добавление книги
+   page.solve_quiz_and_get_code()      # Работа с assert
+   page.should_be_name_equal()     #сравнение наименований
+   page.should_be_price_equal()        #сравнение цен
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.add_to_basket()          # добавление книги
+    page.should_not_be_success_message()    # упадет, как только увидит искомый элемент
 
 
-# def test_guest_should_see_login_link_on_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
 
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.add_to_basket()          # добавление книги
+    page.should_not_be_disappeared_success_message()    # будет ждать до тех пор, пока элемент не исчезнет
 
-# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/'
-#     page = BasketPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-#     page.open()                     # открываем страницу
-#     page.go_to_basket_page()       # переход к корзине
-#     page.should_be_no_product()    # проверка на пустую корзину
-#     page.should_be_text_empty_basket()  # проверка текста Корзина пуста
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/'
+    page = BasketPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                     # открываем страницу
+    page.go_to_basket_page()       # переход к корзине
+    page.should_be_no_product()    # проверка на пустую корзину
+    page.should_be_text_empty_basket()  # проверка текста Корзина пуста
 
 
 class TestUserAddToBasketFromProductPage():
@@ -98,6 +102,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()                      # открываем страницу
         page.should_not_be_disappeared_success_message()    # будет ждать до тех пор, пока элемент не исчезнет
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
